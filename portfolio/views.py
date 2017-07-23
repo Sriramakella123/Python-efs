a978ad9169a152f98e49eb9ded287acb48f9c2fa
Sriramakella123 <vakella@unomaha.edu>
2017-07-22 21:35:22 -0500

    changes for investments

--- portfolio/views.py
+++ portfolio/views.py
@@ -137,7 +137,7 @@ def investment_edit(request, pk):
 
 @login_required
 def investment_delete(request, pk):
-   investment = get_object_or_404(Stock, pk=pk)
+   investment = get_object_or_404(Investment, pk=pk)
    investment.delete()
    investments = Investment.objects.filter(acquired_date__lte=timezone.now())
    return render(request, 'portfolio/investment_list.html', {'investments': investments})
